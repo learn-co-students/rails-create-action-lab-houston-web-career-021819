603de328f3a280bf06196566ee8d558e1fe5ba70
github username <github email address>
2019-03-16 11:30:58 -0500

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,5 +1,5 @@
 class StudentsController < ApplicationController
-  
+  skip_before_action :verify_authenticity_token
   def index
     @students = Student.all
   end
@@ -12,6 +12,15 @@ class StudentsController < ApplicationController
   end
 
   def create
+    student = Student.new
+    student.first_name = params[:first_name]
+    student.last_name = params[:last_name]
+    student.save
+    redirect_to student_path(student)
   end
+
+  #def student_params
+   # params.require(:student).permit(:first_name, :last_name)
+  #end
 
 end
